12dd187a82865952f2d3ebe64c93895d8104e8a7
ethitter <ethitter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-12 01:47:30 +0000

    Date-based Taxonomy Archives v0.3
    * Remove unnecessary uses of `$wpdb->prepare()` that were incorrectly implemented, resulting in PHP warnings when `WP_DEBUG` was enabled.
    * Convert plugin to a singelton pattern instead of relying on a global variable.
    * Protect class variables and expose them only through a magic getter. Allows for more flexibility in adding class variables in the future.
    * Localize large post counts for display, when requested.
    * Correct phpdoc implementation.
    
    Full changelog at https://github.com/ethitter/Date-based-Taxonomy-Archives/commits/master.
    
    
    git-svn-id: https://plugins.svn.wordpress.org/date-based-taxonomy-archives/tags/0.3@711585 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- date-based-taxonomy-archives.php
+++ date-based-taxonomy-archives.php
@@ -4,7 +4,7 @@ Plugin Name: Date-based Taxonomy Archives
 Plugin URI: http://www.ethitter.com/plugins/date-based-taxonomy-archives/
 Description: Add support for date-based taxonomy archives. Render an unordered list of years with months, linked to corresponding date-based taxonomy archive, nested therein.
 Author: Erick Hitter
-Version: 0.2
+Version: 0.3
 Author URI: http://www.ethitter.com/
 
 This program is free software; you can redistribute it and/or modify
@@ -24,29 +24,69 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 class Date_Based_Taxonomy_Archives {
 	/**
+	 * Singleton
+	 */
+	private static $__instance = null;
+
+	/**
 	 * Class variables
 	 */
-	var $defaults = array(
-		'taxonomies' => false,
+	private $defaults = array(
+		'taxonomies'      => false,
 		'show_post_count' => false,
-		'limit' => '',
-		'before' => '',
-		'after' => '',
-		'echo' => true
+		'limit'           => '',
+		'before'          => '',
+		'after'           => '',
+		'echo'            => true,
 	);
 
-	var $cache_key_incrementor = 'incrementor';
-	var $cache_group = 'date_based_taxonomy_archives';
+	private $cache_key_incrementor = 'incrementor';
+	private $cache_group = 'date_based_taxonomy_archives';
 
-	var $filter_archive_links = false;
+	private $filter_archive_links = false;
+
+	/**
+	 * Silence is golden!
+	 */
+	private function __construct() {}
+
+	/**
+	 * Singleton implementation
+	 *
+	 * @uses self::setup
+	 * @return object
+	 */
+	public static function get_instance() {
+		if ( ! is_a( self::$__instance, __CLASS__ ) ) {
+			self::$__instance = new self;
+
+			self::$__instance->setup();
+		}
+
+		return self::$__instance;
+	}
+
+	/**
+	 * Magic getter to provide access to class variables that were public prior to v0.3.
+	 *
+	 * @param string $name
+	 * @return mixed
+	 */
+	public function __get( $name ) {
+		if ( property_exists( $this, $name ) )
+			return $this->$name;
+		else
+			return null;
+	}
 
 	/**
 	 * Register actions and filters
 	 *
-	 * @uses add_action, add_filter
+	 * @uses add_action
+	 * @uses add_filter
 	 * @return null
 	 */
-	function __construct() {
+	private function setup() {
 		add_filter( 'date_based_taxonomy_archives_where', array( $this, 'filter_date_based_taxonomy_archives_where' ), 10, 2 );
 		add_filter( 'date_based_taxonomy_archives_join', array( $this, 'filter_date_based_taxonomy_archives_join' ), 10, 2 );
 		add_filter( 'get_archives_link', array( $this, 'filter_get_archives_link' ) );
@@ -59,8 +99,18 @@ class Date_Based_Taxonomy_Archives {
 	/**
 	 * Render unordered lists of monthly archive links grouped by year
 	 *
+	 * @global $wpdb
+	 * @global $wp_locale
 	 * @param array $args
-	 * @uses $wpdb, $wp_locale, apply_filters, wp_parse_args, absint, this::get_incrementor, wp_cache_get, wp_cache_set, get_month_link, get_archives_link
+	 * @uses apply_filters
+	 * @uses wp_parse_args
+	 * @uses absint
+	 * @uses this::get_incrementor
+	 * @uses wp_cache_get
+	 * @uses wp_cache_set
+	 * @uses number_format_i18n
+	 * @uses get_month_link
+	 * @uses get_archives_link
 	 * @return string or false
 	 */
 	function get_archives( $args = array() ) {
@@ -70,16 +120,16 @@ class Date_Based_Taxonomy_Archives {
 		$args = wp_parse_args( $args, $this->defaults );
 		extract( $args );
 
-		//Build query
+		// Build query
 		$where = apply_filters( 'date_based_taxonomy_archives_where', "WHERE post_type = 'post' AND post_status = 'publish'", $args );
 		$join = apply_filters( 'date_based_taxonomy_archives_join', '', $args );
 
 		if ( is_numeric( $limit ) )
 			$limit = ' LIMIT ' . absint( $limit );
 
-		$query = $wpdb->prepare( "SELECT YEAR(post_date) AS `year`, MONTH(post_date) AS `month`, count(ID) as posts FROM $wpdb->posts $join $where GROUP BY YEAR(post_date), MONTH(post_date) ORDER BY post_date DESC $limit" );
+		$query = "SELECT YEAR(post_date) AS `year`, MONTH(post_date) AS `month`, count(ID) as posts FROM $wpdb->posts $join $where GROUP BY YEAR(post_date), MONTH(post_date) ORDER BY post_date DESC $limit";
 
-		//Generate cache key, check cache, query DB if necessary and cache results
+		// Generate cache key, check cache, query DB if necessary and cache results
 		$cache_key = $this->get_incrementor() . md5( $query );
 
 		if ( ! $results = wp_cache_get( $cache_key, $this->cache_group ) ) {
@@ -88,15 +138,15 @@ class Date_Based_Taxonomy_Archives {
 			wp_cache_set( $cache_key, $results, $this->cache_group );
 		}
 
-		//Bail out if necessary data isn't available
+		// Bail out if necessary data isn't available
 		if ( ! is_array( $results ) || empty( $results ) )
 			return false;
 
-		//Render archive
+		// Render archive
 		$output = '<ul>';
 		$cy = false;
 
-		//Alias $after for use inside of foreach
+		// Alias $after for use inside of foreach
 		$_after = $after;
 
 		foreach ( $results as $result ) {
@@ -114,7 +164,7 @@ class Date_Based_Taxonomy_Archives {
 
 			$text = $wp_locale->get_month( $result->month );
 
-			$after = $show_post_count ? '&nbsp;(' . absint( $result->posts ) . ')' . $_after : $_after;
+			$after = $show_post_count ? '&nbsp;(' . number_format_i18n( absint( $result->posts ) ) . ')' . $_after : $_after;
 
 			$output .= get_archives_link( $url, $text, 'html', $before, $after );
 		}
@@ -124,7 +174,7 @@ class Date_Based_Taxonomy_Archives {
 
 		$output .= '</ul>';
 
-		//Reset archive links filter indicator
+		// Reset archive links filter indicator
 		$this->filter_archive_links = false;
 
 		if ( $echo )
@@ -136,9 +186,15 @@ class Date_Based_Taxonomy_Archives {
 	/**
 	 * Filter where clause used in this::get_archives
 	 *
+	 * @global $wpdb
 	 * @param string $where
 	 * @param array $args
-	 * @uses $wpdb, apply_filters, wp_parse_args, is_category, is_tag, is_tax, get_queried_object
+	 * @uses apply_filters
+	 * @uses wp_parse_args
+	 * @uses is_category
+	 * @uses is_tag
+	 * @uses is_tax
+	 * @uses get_queried_object
 	 * @filter date_based_taxonomy_archives_where
 	 * @return string
 	 */
@@ -157,8 +213,7 @@ class Date_Based_Taxonomy_Archives {
 
 			if ( is_object( $queried_object ) && property_exists( $queried_object, 'term_taxonomy_id' ) )
 				$where .= $wpdb->prepare( ' AND dbtrtr.term_taxonomy_id = %d', $queried_object->term_taxonomy_id );
-		}
-		elseif ( is_array( $taxonomies ) ) {
+		} elseif ( is_array( $taxonomies ) ) {
 			$queried_object = get_queried_object();
 
 			if ( is_object( $queried_object ) && property_exists( $queried_object, 'term_taxonomy_id' ) && property_exists( $queried_object, 'taxonomy' ) && in_array( $queried_object->taxonomy, $taxonomies ) )
@@ -171,9 +226,14 @@ class Date_Based_Taxonomy_Archives {
 	/**
 	 * Filter join clause used in this::get_archives
 	 *
+	 * @global $wpdb
 	 * @param string $join
 	 * @param array $args
-	 * @uses $wpdb, apply_filters, wp_parse_args, is_category, is_tag, is_tax
+	 * @uses apply_filters
+	 * @uses wp_parse_args
+	 * @uses is_category
+	 * @uses is_tag
+	 * @uses is_tax
 	 * @filter date_based_taxonomy_archives_join
 	 * @return string
 	 */
@@ -189,7 +249,7 @@ class Date_Based_Taxonomy_Archives {
 			( $taxonomies == 'custom' && ! is_category() && ! is_tag() && is_tax() ) ||
 			is_array( $taxonomies )
 		) {
-			$join .= $wpdb->prepare( " INNER JOIN {$wpdb->term_relationships} AS dbtrtr on dbtrtr.object_id = {$wpdb->posts}.ID" );
+			$join .= " INNER JOIN {$wpdb->term_relationships} AS dbtrtr on dbtrtr.object_id = {$wpdb->posts}.ID";
 
 			$this->filter_archive_links = true;
 		}
@@ -200,8 +260,15 @@ class Date_Based_Taxonomy_Archives {
 	/**
 	 * Filter get_archives_link output to inject taxonomy and term slugs
 	 *
+	 * @global $wp_rewrite
 	 * @param string $link_html
-	 * @uses $wp_rewrite, get_queried_object, is_wp_error, path_join, trailingslashit, home_url, get_taxonomy, add_query_arg
+	 * @uses get_queried_object
+	 * @uses is_wp_error
+	 * @uses path_join
+	 * @uses trailingslashit
+	 * @uses home_url
+	 * @uses get_taxonomy
+	 * @uses add_query_arg
 	 * @filter get_archives_link
 	 * @return string
 	 */
@@ -215,17 +282,16 @@ class Date_Based_Taxonomy_Archives {
 				$exploded = explode( "'", $link_html );
 
 				if ( $wp_rewrite->using_permalinks() && array_key_exists( $queried_object->taxonomy, $wp_rewrite->extra_permastructs ) ) {
-					$term_rewrite = preg_replace( '#%[^%]+%#i', $queried_object->slug, $wp_rewrite->extra_permastructs[ $queried_object->taxonomy ][ 'struct' ] );
-					$term_rewrite = substr( $term_rewrite, 1 ); //Drop leading slash, otherwise path_join misinterprets request
+					$term_rewrite = preg_replace( '#%[^%]+%#i', $queried_object->slug, $wp_rewrite->extra_permastructs[$queried_object->taxonomy]['struct'] );
+					$term_rewrite = substr( $term_rewrite, 1 ); // Drop leading slash, otherwise path_join misinterprets request
 					$term_rewrite = path_join( $term_rewrite, 'date' );
 
-					$exploded[ 1 ] = str_replace( trailingslashit( home_url() ), trailingslashit( path_join( home_url(), $term_rewrite ) ), $exploded[ 1 ] );
-				}
-				else {
+					$exploded[1] = str_replace( trailingslashit( home_url() ), trailingslashit( path_join( home_url(), $term_rewrite ) ), $exploded[1] );
+				} else {
 					$taxonomy = get_taxonomy( $queried_object->taxonomy );
 
 					if ( is_object( $taxonomy ) && ! is_wp_error( $taxonomy ) )
-						$exploded[ 1 ] = add_query_arg( $taxonomy->query_var, $queried_object->slug, $exploded[ 1 ] );
+						$exploded[1] = add_query_arg( $taxonomy->query_var, $queried_object->slug, $exploded[1] );
 				}
 
 				$link_html = implode( "'", $exploded );
@@ -250,7 +316,7 @@ class Date_Based_Taxonomy_Archives {
 			$rules = array();
 
 			foreach ( $taxonomies as $taxonomy )
-				$rules[ $taxonomy->rewrite[ 'slug' ] . '/(.+?)/date/([0-9]{4})/([0-9]{1,2})/?(page/?([0-9]{1,})/?)?$' ] = 'index.php?' . $taxonomy->query_var . '=$matches[1]&year=$matches[2]&monthnum=$matches[3]&paged=$matches[5]';
+				$rules[$taxonomy->rewrite['slug'] . '/(.+?)/date/([0-9]{4})/([0-9]{1,2})/?(page/?([0-9]{1,})/?)?$'] = 'index.php?' . $taxonomy->query_var . '=$matches[1]&year=$matches[2]&monthnum=$matches[3]&paged=$matches[5]';
 
 			$wp_rewrite->rules = $rules + $wp_rewrite->rules;
 		}
@@ -259,7 +325,8 @@ class Date_Based_Taxonomy_Archives {
 	/**
 	 * Return cache incrementor. To invalidate caches, incrementor is deleted via this::action_transition_post_status.
 	 *
-	 * @uses wp_cache_get, wp_cache_set
+	 * @uses wp_cache_get
+	 * @uses wp_cache_set
 	 * @return int
 	 */
 	function get_incrementor() {
@@ -270,7 +337,7 @@ class Date_Based_Taxonomy_Archives {
 			wp_cache_set( $this->cache_key_incrementor, $incrementor, $this->cache_group );
 		}
 
-		return (int)$incrementor;
+		return (int) $incrementor;
 	}
 
 	/**
@@ -287,22 +354,20 @@ class Date_Based_Taxonomy_Archives {
 			wp_cache_delete( $this->cache_key_incrementor, $this->cache_group );
 	}
 }
-global $date_based_taxonomy_archives;
-if ( ! is_a( $date_based_taxonomy_archives, 'Date_Based_Taxonomy_Archives' ) )
-	$date_based_taxonomy_archives = new Date_Based_Taxonomy_Archives;
+Date_Based_Taxonomy_Archives::get_instance();
+
+/**
+ * Alias global variable used to hold class prior to singleton implementation in v0.3.
+ */
+$GLOBALS['date_based_taxonomy_archives'] = Date_Based_Taxonomy_Archives::get_instance();
 
 /**
  * Render unordered lists of monthly archive links grouped by year
  *
  * @param array $args
- * @uses $date_based_taxonomy_archives
+ * @uses Date_Based_Taxonomy_Archives::get_instance
  * @return string or false
  */
 function date_based_taxonomy_archives( $args = array() ) {
-	global $date_based_taxonomy_archives;
-	if ( ! is_a( $date_based_taxonomy_archives, 'Date_Based_Taxonomy_Archives' ) )
-		$date_based_taxonomy_archives = new Date_Based_Taxonomy_Archives;
-
-	return $date_based_taxonomy_archives->get_archives( $args );
+	return Date_Based_Taxonomy_Archives::get_instance()->get_archives( $args );
 }
-?>
